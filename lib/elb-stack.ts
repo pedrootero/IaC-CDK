e32c3464be11e26d4f3eefd4ce55994a2c7599ab
Pedro <pedro@PEDROOTERO.cooxupe.com.br>
2022-10-19 15:48:11 -0300

    add/comments

--- lib/elb-stack.ts
+++ lib/elb-stack.ts
@@ -6,8 +6,18 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { InstanceType } from 'aws-cdk-lib/aws-ec2';
 import { readFileSync } from 'fs';
 
+//Classe PropsElb criada para não precisar passar um por um os paramentros dentro do construtor da classe ELB
+/* 
+interface PropsElb extends cdk.StackProps {
+	readonly vpc: ec2.Vpc;
+	readonly instanceType: InstanceType;
+} */
+
+// Devido a estar usando o obejto instanceType já criado em outra stack/classe, é necessário passa-la como parâmetro no construtor do ELB.
+
+//Como o ELB extends o cdk.stack é necessário passar os parametros criados mais o cdk.StackProps
 export class Elb extends cdk.Stack {
-	constructor(scope: Construct, id: string, vpc: ec2.Vpc, instaceType: InstanceType, props?: cdk.StackProps) {
+	constructor(scope: Construct, id: string, vpc: ec2.Vpc, instanceType: InstanceType, props?: cdk.StackProps) {
 		super(scope, id, props);
 
 		const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
@@ -27,10 +37,15 @@ export class Elb extends cdk.Stack {
 
 		// Create an AutoScaling group and add it as a load balancing
 		// target to the listener.
+
+		/* Como esta sendo criado o objeto  machineImage agora dentro da classe não é necessário passa-lo como parâmetro dentro do construtor do ELB, apenas criado dentro das propriedades
+        do props do metodo  AutoScalingGroup
+         */
+
 		const asg = new AutoScalingGroup(this, 'autoscaling-group', {
 			vpc: vpc,
 			machineImage: ec2.MachineImage.latestAmazonLinux(),
-			instanceType: instaceType,
+			instanceType: instanceType,
 		});
 		const userDataScript = readFileSync('./lib/user-data.sh', 'utf8');
 
--- lib/user-data.sh
+++ lib/user-data.sh
@@ -3,4 +3,3 @@ sudo su
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "<h1>Serviço rodando $(hostname -f)</h1>" > /var/www/html/index.html
